f7947f0e6d453aa798773278611c0270ed74ef6e
Arka Roy <arka243roy@googlemail.com>
2019-02-25 16:38:55 +0000

    Add files via upload

---
+++ javascript/level_one/Part3.js
@@ -0,0 +1,4 @@
+var pounds=prompt("Enter value in pounds(lb)");
+var kilograms=pounds*0.454;
+alert("It is "+kilograms+" in kilograms");
+console.log("Conversion Completed!");
---
+++ javascript/level_one/level_one_basics.js
@@ -0,0 +1,14 @@
+alert("Hello World!")
+// anything here is a comment
+var bankAccount = 100;
+var deposit = 50;
+var total=bankAccount + deposit;
+var greeting  = "Welcome back: ";
+var name = "Arka";
+alert(greeting+name);
+//undefined
+var myVariable;
+//null
+var bonus=null;
+console.log("Hey I'm in the console!")
+var age = prompt("Enter your age:")
